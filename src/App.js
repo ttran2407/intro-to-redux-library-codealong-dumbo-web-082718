d0d7cd4b4455e6d24619b19fefe764387823f146
github username <github email address>
2018-12-21 17:55:57 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 
+
 class App extends Component {
 	handleOnClick = event => {
 		this.props.increaseCount();
